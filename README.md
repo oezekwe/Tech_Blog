4b6f33a1f0ff457c8effa0ff70aeb0fad519b5f5
Obinna Ezekwe <ozekwe@gmail.com>
2021-01-17 23:40:54 -0600

    Written in ReadMe file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# This is suppose to be a blog site where you can publish articles, posts, and opinions.
+
 ## Link: https://infinite-taiga-44126.herokuapp.com/ 
--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -3,6 +3,14 @@ const sequelize= require('../config/connection');
 const {User, Blog, Comment}= require('../models');
 
 router.get('/', (req, res) => {
+    Blog.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            
+        ]
+    })
     res.render('dashboard', { loggedIn: true });
 });
 
